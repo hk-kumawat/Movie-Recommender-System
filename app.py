b785a5f67f5a49465d11e3e97a1d4e79b75a5e61
hk-kumawat <harshalkumawat100@gmail.com>
2025-02-13 23:48:50 +0530

    improved and bug fixes

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ st.markdown("""
     <p style='text-align: center; color: #7f8c8d; font-size: 2.5rem; margin-top: 0;'>
         Just pick a title and let us do the magic ✨
     </p>
-    <div style="border-bottom: 2px solid #eee; margin: 1rem 0;"></div>
+
 """, unsafe_allow_html=True)
 
 # ------------------------------
@@ -451,7 +451,7 @@ with st.sidebar:
 # ------------------------------
 st.markdown("<div style='border-top: 2px solid #eee; margin: 2rem 0;'></div>", unsafe_allow_html=True)
 st.markdown("""
-    <div style='text-align: center; color: #888; padding: 10px; font-size: 0.9rem;'>
-        |  Made with ♥️ by Harshal Kumawat  |<br>
+    <div style='text-align: center; color: #888; padding: 10px; font-size: 1rem;'>
+          Made with ♥️ by  <strong>Harshal Kumawat</strong>
     </div>
 """, unsafe_allow_html=True)
